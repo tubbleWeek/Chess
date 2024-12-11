9015473622f8321c2310ad209fca7c27513d5894
tubbleWeek <wchastek@gmail.com>
2024-12-11 11:51:20 -0600

    changed netork params

--- r_learning/q_learn.py
+++ r_learning/q_learn.py
@@ -60,29 +60,29 @@ def board_to_tensor(board: chess.Board):
 # q_network = ChessQNetwork()
 # optimizer = optim.Adam(q_network.parameters(), lr=LEARNING_RATE)
 # memory = deque(maxlen=MEMORY_SIZE)
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-q_network = ChessQNetwork().to(DEVICE)
-q_network.load_state_dict(torch.load("./r_learning/q_learning_model_final.pth"))
+# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# q_network = ChessQNetwork().to(DEVICE)
+# q_network.load_state_dict(torch.load("./r_learning/q_learning_model_final.pth"))
 # # Helper functions for Q-learning
-def select_move(board, epsilon):
-    """Select a move using an epsilon-greedy policy."""
-    legal_moves = list(board.legal_moves)
-    if random.random() < epsilon:
-        # Explore: Choose a random move
-        return random.choice(legal_moves)
-    else:
-        # Exploit: Choose the best move based on Q-value
-        best_move = None
-        best_q_value = -float("inf")
-        for move in legal_moves:
-            board.push(move)
-            # print(q_network)
-            q_value = q_network(board_to_tensor(board).to(DEVICE)).item()
-            board.pop()
-            if q_value > best_q_value:
-                best_q_value = q_value
-                best_move = move
-        return best_move
+# def select_move(board, epsilon):
+#     """Select a move using an epsilon-greedy policy."""
+#     legal_moves = list(board.legal_moves)
+#     if random.random() < epsilon:
+#         # Explore: Choose a random move
+#         return random.choice(legal_moves)
+#     else:
+#         # Exploit: Choose the best move based on Q-value
+#         best_move = None
+#         best_q_value = -float("inf")
+#         for move in legal_moves:
+#             board.push(move)
+#             # print(q_network)
+#             q_value = q_network(board_to_tensor(board).to(DEVICE)).item()
+#             board.pop()
+#             if q_value > best_q_value:
+#                 best_q_value = q_value
+#                 best_move = move
+#         return best_move
 
 
 # def train_q_network(batch):
